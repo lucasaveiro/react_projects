fddd155ee7a49cc6b4bde92a0b06fdfbb8a61da4
Lucas Aveiro <lucas.n.aveiro@gmail.com>
2023-02-27 22:03:23 -0300

    edited from functionApp to a class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +24,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
