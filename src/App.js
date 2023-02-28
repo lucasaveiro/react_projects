0557d85f96e32f6a1e966ccbc80359770a2cb3cc
lucasaveiro <lucas.n.aveiro@gmail.com>
2023-02-28 20:24:27 -0300

    list generated by map of an array

--- src/App.js
+++ src/App.js
@@ -4,26 +4,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
 
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      monsters: [
+      {
+        name: 'Linda',
+        id: '1',
+      },
+      {
+        name: 'Frank',
+        id: '2',
+      },
+      {
+        name: 'Jacky',
+        id: '3',
+      },
+      {
+        name: 'Anmre',
+        id: '4',
+      },
+      ]
+    };
+      
+
+    };
+
+  render() {
+    return <div className="App">
+    {
+      this.state.monsters.map((monster)=>{
+        return <h1 key={monster.name}>{monster.name}</h1>;
+      })
+    }
+      </div>
   }
 }
 
