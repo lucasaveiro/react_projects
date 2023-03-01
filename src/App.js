4c8c52732023b2a9f45a9bd5e47415ce120097f6
lucasaveiro <115370072+lucasaveiro@users.noreply.github.com>
2023-03-01 16:33:35 -0300

    add comments

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
     };
   }
 
+  // gets from API the infos of users to display the names
+  // When it receives the response, it converts to JSON
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
@@ -23,12 +25,20 @@ class App extends Component {
       .catch(error => console.error(error))
   }
 
+  // This 'event' is an object that contains information of the 'onChange' event
+  // 'event.target.value' is the value inside the input when the 'event'->'onChange' changes
+  // this.setState is necessary to update the state and the react will re-render with this change
   handleChange = (event) => {
     this.setState({ searchField: event.target.value});
   }
 
   render() {
+    // This line makes the consts 'monster' and 'searchField' possible to use without the 'this.state.monsters'
+    // and 'this.state.searchField'
   const { monsters, searchField } = this.state;
+
+  // ".filter" waits true or false of (if monster.name includes what is at searchField variable)
+  // If its true, the 'filter' save that monster on the const filteredMonsters that is an array
   const filteredMonsters = monsters.filter(monster =>
     monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
@@ -38,6 +48,7 @@ class App extends Component {
       type='search' 
       placeholder='search monsters' 
       onChange={this.handleChange} />
+
     {
       filteredMonsters.map((monster)=>{
         return <h1 key={monster.name}>{monster.name}</h1>;
